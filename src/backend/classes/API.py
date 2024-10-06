4b530731a3cb0a29a670379996a739e70bd4858c
Malxyzia <hlam@live.co.uk>
2024-10-06 16:58:46 +1100

    Added Service and API classes

---
+++ src/backend/classes/API.py
@@ -0,0 +1,26 @@
+from typing import List
+from APIOverflow.src.backend.classes.Service import Service
+
+class API(Service):
+
+    '''
+        API class which represents an API uploaded to the platform.
+
+        Inherits from Service
+    
+    '''
+
+    def __init__(self,
+                 sid: str,
+                 name: str,
+                 owner: str,
+                 icon_url: str,
+                 description: str,
+                 tags: List[str]) -> None:
+        super().__init__(sid, name, owner, icon_url, description, tags, 'api')
+    
+    def get_type(self):
+        '''
+            Returns 'api'
+        '''
+        return self._type
