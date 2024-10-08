51b6341fe31645be7cbe816e8bc458bbd212ed6c
Vincent Yu <sydney.vincent.yu@gmail.com>
2024-10-09 04:43:16 +1100

    removed extra comments

--- src/backend/app.py
+++ src/backend/app.py
@@ -12,19 +12,16 @@ from src.backend.database import db
 
 app = FastAPI()
 
-# Define allowed origins
 origins = [
-    "http://localhost:3000",  # Allow your frontend to make requests
-    # Add more origins here if needed
+    "http://localhost:3000",
 ]
 
-# Add the CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # Allow only specified origins
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"], 
+    allow_headers=["*"],
 )
 
 manager = _manager.get_manager()
