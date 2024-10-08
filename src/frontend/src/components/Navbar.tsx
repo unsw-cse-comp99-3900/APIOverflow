b7f69c6da8340adfbd843839e8e272824e58e8ed
Vincent Yu <sydney.vincent.yu@gmail.com>
2024-10-08 17:38:21 +1100

    modified NavBar Layout

--- src/frontend/src/components/Navbar.tsx
+++ src/frontend/src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-blue-900 border-b border-gray-100 fixed top-0 w-full z-50">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div className="m-auto px-20">
         <div className="flex h-20 items-center justify-between">
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
             <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
