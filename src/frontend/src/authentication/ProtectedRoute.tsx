eb12352d458164b090f173d2ff67069a5e7e042f
Vincent Yu <sydney.vincent.yu@gmail.com>
2024-10-10 03:09:47 +1100

    removed toast notif for protected route

--- src/frontend/src/authentication/ProtectedRoute.tsx
+++ src/frontend/src/authentication/ProtectedRoute.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../authentication/AuthProvider';
-import { toast } from 'react-toastify';
 
 const ProtectedRoute = () => {
   const auth = useAuth();
   const { isLoggedIn } = auth!;
 
   if (!isLoggedIn) {
-    toast.error('You must be logged in to view this page');
     return <Navigate to="/login" replace />; // Redirect to login if not logged in
   }
   // If authenticated, render the children components (Outlet is for nested routes)
