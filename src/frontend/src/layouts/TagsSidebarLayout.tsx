166aa2645bec4e04933c43c500f40a09f09bbe7b
Vincent Yu <sydney.vincent.yu@gmail.com>
2024-10-25 23:09:54 +1100

    added types for each page

--- src/frontend/src/layouts/TagsSidebarLayout.tsx
+++ src/frontend/src/layouts/TagsSidebarLayout.tsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
-import { Outlet } from 'react-router-dom';
-import TagsSideBar from '../components/TagsSideBar';
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import TagsSideBar from "../components/TagsSideBar";
 
-
-const TagsSidebarLayout = () => {
+const TagsSidebarLayout: React.FC = () => {
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
   return (
     <div className="flex">
-      <TagsSideBar selectedTags={selectedTags} setSelectedTags={setSelectedTags} />
+      <TagsSideBar
+        selectedTags={selectedTags}
+        setSelectedTags={setSelectedTags}
+      />
       <section className="flex-1 ml-80">
-        <Outlet context={{ selectedTags }}/>
+        <Outlet context={{ selectedTags }} />
       </section>
     </div>
   );
