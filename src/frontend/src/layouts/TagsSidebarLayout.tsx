232ddf6a4579732c2ae97c81867f67c47ff1ec66
Vincent Yu <sydney.vincent.yu@gmail.com>
2024-10-25 23:27:40 +1100

    removed unecessary import

--- src/frontend/src/layouts/TagsSidebarLayout.tsx
+++ src/frontend/src/layouts/TagsSidebarLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Outlet, useOutletContext } from "react-router-dom";
 import TagsSideBar from "../components/TagsSideBar";
 
