e2a78a0ad0aa35a2786ce8beffef45483058fcd5
Vincent Yu <sydney.vincent.yu@gmail.com>
2024-10-26 14:21:21 +1100

    fixed tagging fully implemented

--- src/frontend/src/components/TagsOverlay.tsx
+++ src/frontend/src/components/TagsOverlay.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Tag } from "../types/miscTypes";
+import { getTags } from "../services/apiServices";
+import { FaTrash } from "react-icons/fa";
 
-interface FullScreenOverlayProps {
+interface TagsOverlayProps {
   isOpen: boolean;
   onClose: () => void;
+  selectedTags: string[];
+  setSelectedTags: React.Dispatch<React.SetStateAction<string[]>>;
 }
 
-const TagsOverlay: React.FC<FullScreenOverlayProps> = ({ isOpen, onClose }) => {
+const TagsOverlay: React.FC<TagsOverlayProps> = ({
+  isOpen,
+  onClose,
+  selectedTags,
+  setSelectedTags,
+}) => {
+  const tagClass = ({ tag }: { tag: string }) =>
+    selectedTags.includes(tag)
+      ? "relative border-blue-800 border-2 bg-blue-800 text-white  flex items-center justify-center rounded-md text-sm font-semibold px-3 py-1 mx-1 my-1"
+      : "relative border-blue-800 border-2 bg-white hover:bg-blue-800 text-blue-800 hover:text-white flex items-center justify-center rounded-md text-sm font-semibold px-3 py-1 mx-1 my-1";
+
+  const toggleTag = (tag: string) => {
+    if (selectedTags.includes(tag)) {
+      setSelectedTags(selectedTags.filter((t) => t !== tag));
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  }
+
+  const [tags, setTags] = useState<Tag[]>([]);
+  useEffect(() => {
+    const fetchApis = async () => {
+      try {
+        const data = await getTags();
+        setTags(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      }
+    };
+    fetchApis();
+  }, []);
+
   if (!isOpen) return null;
 
   return (
@@ -18,13 +54,21 @@ const TagsOverlay: React.FC<FullScreenOverlayProps> = ({ isOpen, onClose }) => {
         >
           &times;
         </button>
-        
-        {/* Overlay Content */} 
+
+        {/* Overlay Content */}
         <div className="text-left">
           <h2 className="text-2xl font-semibold text-blue-800 mb-4">Tags</h2>
-          <p>This is an example of a full-screen overlay using Tailwind CSS.</p>
-          
-          {/* Add more content as per your design */}
+          <div className="flex flex-wrap">
+            {tags.map((tag) => (
+              <button
+                type="button" // Prevent form submission
+                onClick = {() => toggleTag(tag)}
+                className={tagClass({ tag })}
+              >
+                {tag}
+              </button>
+            ))}
+          </div>
         </div>
       </div>
     </div>
