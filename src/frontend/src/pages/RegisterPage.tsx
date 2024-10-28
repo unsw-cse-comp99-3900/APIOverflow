aaf0176329724b1f30e43aec546192303aa45aa1
Vincent Yu <sydney.vincent.yu@gmail.com>
2024-10-29 01:41:14 +1100

    enabled email verification

--- src/frontend/src/pages/RegisterPage.tsx
+++ src/frontend/src/pages/RegisterPage.tsx
@@ -33,20 +33,21 @@ const RegisterPage: React.FC = () => {
     };
     await userRegister(newUser);
     setIsLoading(true);
-    try {
-      const credentials: LoginModel = {
-        username,
-        password,
-      };
-      const token = await userLogin(credentials);
-      login(token);
-      setIsLoading(false);
-      console.log("Login successful:", { username, password });
-      navigate("/apis");
-    } catch (error) {
-      setError("Invalid credentials");
-      setIsLoading(false);
-    }
+    navigate("/login");
+    // try {
+    //   const credentials: LoginModel = {
+    //     username,
+    //     password,
+    //   };
+    //   const token = await userLogin(credentials);
+    //   login(token);
+    //   setIsLoading(false);
+    //   console.log("Login successful:", { username, password });
+    //   navigate("/apis");
+    // } catch (error) {
+    //   setError("Invalid credentials");
+    //   setIsLoading(false);
+    // }
   };
 
   return (
