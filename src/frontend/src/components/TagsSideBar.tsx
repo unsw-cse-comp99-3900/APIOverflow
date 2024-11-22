9328a1ccc5899b787ccccfea19f7967cec8e3076
Malxyzia <hlam@live.co.uk>
2024-11-22 12:12:16 +1100

    Fixed some things, tags still not working

--- src/frontend/src/components/TagsSideBar.tsx
+++ src/frontend/src/components/TagsSideBar.tsx
@@ -13,6 +13,9 @@ const TagsSideBar: React.FC = () => {
   const [displayNum, setDisplayNum] = useState<number>(0);
   const [displayAll, setDisplayAll] = useState<boolean>(false);
 
+  let displayNum_c = 0;
+  let cTagsNum_c = 0;
+
   useEffect(() => {
     const fetchApis = async () => {
       try {
@@ -23,17 +26,24 @@ const TagsSideBar: React.FC = () => {
         setCTags(cData);
         setCTagsNum(cData.length);
 
+
         // Determine how many tags should be shown
         if (cData.length > 10) {
+          console.log("Ran here");
           setDisplayNum(10);
+          displayNum_c = 10;
+
         } else {
+          console.log("Ran there");
           setDisplayNum(cData.length);
+          displayNum_c = cData.length;
         };
-        toast.error(`Display Num: ${displayNum} | Tags: ${cTagsNum} | CData ${cData.length}`);
+        // toast.error(`Display Num: ${displayNum} | Tags: ${cTagsNum} | CData ${cData.length}`);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
+
     fetchApis();
   }, []);
 
@@ -47,7 +57,7 @@ const TagsSideBar: React.FC = () => {
   const toggleDisplayAll = () => {
     setDisplayAll(!displayAll);
   };
-
+  console.log(`Display Num: ${displayNum_c} | Tags: ${cTagsNum_c}`);
   return (
     <aside className="fixed left-0 w-80 h-full px-4 py-8 overflow-y-auto bg-white border-r dark:bg-gray-900 dark:border-gray-700">
       <div className="flex justify-between items-center px-4">
@@ -88,7 +98,7 @@ const TagsSideBar: React.FC = () => {
         </h4>
       </div>
       <div className="grid grid-cols-1 gap-1">
-        {cTags.slice(displayNum).map((tagInfo) => (
+        {cTags.slice(displayNum_c).map((tagInfo) => (
           <span
             key={tagInfo.tag}
             className="flex items-center px-4 py-2 text-gray-900 bg-white rounded-lg dark:bg-gray-800 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 group mt-3 hover:cursor-pointer"
@@ -100,9 +110,9 @@ const TagsSideBar: React.FC = () => {
             </span>
           </span>
         ))}
-        {displayAll && (
+        {displayAll === true && (
                 <div className="grid grid-cols-1 gap-1">
-                {cTags.slice(displayNum, cTagsNum).map((tagInfo) => (
+                {cTags.slice(displayNum_c, cTagsNum_c).map((tagInfo) => (
                   <span
                     key={tagInfo.tag}
                     className="flex items-center px-4 py-2 text-gray-900 bg-white rounded-lg dark:bg-gray-800 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 group mt-3 hover:cursor-pointer"
@@ -118,18 +128,21 @@ const TagsSideBar: React.FC = () => {
         )}
       </div>
       <div className="mb-5 mt-5"></div>
-      {/* Conditionally render if more than 10 custom tags */}
-        {cTagsNum > 10 && (
-          <div className="flex justify-end">
-            <button className="text-white px-1 py-2 font-semibold bg-blue-500 rounded-lg hover:bg-blue-800 hover:cursor-pointer mt-3"
-              onClick={toggleDisplayAll}>
-              {`${displayAll
-                ? "Show less"
-                : "Show all"
-                }`}
-            </button>
-          </div>
-        )}
+      <div>
+        {/* Conditionally render if more than 10 custom tags */}
+        {cTagsNum_c > 10 && (
+            <div className="flex justify-end">
+              <button className="text-white px-1 py-2 font-semibold bg-blue-500 rounded-lg hover:bg-blue-800 hover:cursor-pointer mt-3"
+                onClick={toggleDisplayAll}>
+                {`${displayAll
+                  ? "Show less"
+                  : "Show all"
+                  }`}
+              </button>
+            </div>
+          )}
+      </div>
+
 
     </aside>
   );
